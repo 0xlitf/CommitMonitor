eb404961b966191d8d614bc0129a583218a35485
steveking <steveking@095afe11-7fa7-41de-8b0b-c5876c4b31aa>
2009-03-21 16:11:31 +0000

    Tag the 1.5.0 release

---
+++ tags/version-1.5.0/src/OptionsDlg.cpp
@@ -0,0 +1,200 @@
+// CommitMonitor - simple checker for new commits in svn repositories
+
+// Copyright (C) 2007-2009 - Stefan Kueng
+
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software Foundation,
+// 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+//
+#include "StdAfx.h"
+#include "Resource.h"
+#include "OptionsDlg.h"
+#include "Registry.h"
+#include "AppUtils.h"
+#include <string>
+#include <Commdlg.h>
+
+using namespace std;
+
+COptionsDlg::COptionsDlg(HWND hParent)
+{
+	m_hParent = hParent;
+}
+
+COptionsDlg::~COptionsDlg(void)
+{
+}
+
+LRESULT COptionsDlg::DlgFunc(HWND hwndDlg, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+	UNREFERENCED_PARAMETER(lParam);
+	switch (uMsg)
+	{
+	case WM_INITDIALOG:
+		{
+			InitDialog(hwndDlg, IDI_COMMITMONITOR);
+
+			AddToolTip(IDC_AUTOSTART, _T("Starts the CommitMonitor automatically when Windows starts up."));
+			AddToolTip(IDC_TASKBAR_ALWAYSON, _T("If disabled, the taskbar icon is only shown if new commits are available.\nThe CommitMonitor can be shown by 'starting' it again."));
+			AddToolTip(IDC_DIFFVIEWER, _T("Path to a viewer for unified diff files."));
+			AddToolTip(IDC_DIFFVIEWERLABEL, _T("Path to a viewer for unified diff files."));
+			AddToolTip(IDC_ANIMATEICON, _T("Animates the system tray icon as long as there are unread commits"));
+			AddToolTip(IDC_USETSVN, _T("If TortoiseSVN is installed, use it for showing the differences of commits"));
+			AddToolTip(IDC_CHECKNEWER, _T("Automatically check for newer versions of CommitMonitor"));
+
+			// initialize the controls
+			bool bShowTaskbarIcon = !!(DWORD)CRegStdWORD(_T("Software\\CommitMonitor\\TaskBarIcon"), FALSE);
+			bool bStartWithWindows = !wstring(CRegStdString(_T("Software\\Microsoft\\Windows\\CurrentVersion\\Run\\CommitMonitor"))).empty();
+			bool bAnimateIcon = !!CRegStdWORD(_T("Software\\CommitMonitor\\Animate"), TRUE);
+			bool bPlaySound = !!CRegStdWORD(_T("Software\\CommitMonitor\\PlaySound"), TRUE);
+			bool bUseTSVN = !!CRegStdWORD(_T("Software\\CommitMonitor\\UseTSVN"), TRUE);
+			CRegStdString diffViewer = CRegStdString(_T("Software\\CommitMonitor\\DiffViewer"));
+			CRegStdString notifySound = CRegStdString(_T("Software\\CommitMonitor\\NotificationSound"));
+			CRegStdWORD updatecheck = CRegStdWORD(_T("Software\\CommitMonitor\\CheckNewer"), TRUE);
+			CRegStdWORD numlogs = CRegStdWORD(_T("Software\\CommitMonitor\\NumLogs"), 30);
+			TCHAR numBuf[30] = {0};
+			_stprintf_s(numBuf, 30, _T("%ld"), DWORD(numlogs));
+			SendDlgItemMessage(*this, IDC_TASKBAR_ALWAYSON, BM_SETCHECK, bShowTaskbarIcon ? BST_CHECKED : BST_UNCHECKED, NULL);
+			SendDlgItemMessage(*this, IDC_AUTOSTART, BM_SETCHECK, bStartWithWindows ? BST_CHECKED : BST_UNCHECKED, NULL);
+			SendDlgItemMessage(*this, IDC_ANIMATEICON, BM_SETCHECK, bAnimateIcon ? BST_CHECKED : BST_UNCHECKED, NULL);
+			SendDlgItemMessage(*this, IDC_USETSVN, BM_SETCHECK, bUseTSVN ? BST_CHECKED : BST_UNCHECKED, NULL);
+			SetDlgItemText(*this, IDC_DIFFVIEWER, wstring(diffViewer).c_str());
+			SetDlgItemText(*this, IDC_NOTIFICATIONSOUNDPATH, wstring(notifySound).c_str());
+			SendDlgItemMessage(*this, IDC_NOTIFICATIONSOUND, BM_SETCHECK, bPlaySound ? BST_CHECKED : BST_UNCHECKED, NULL);
+			SendDlgItemMessage(*this, IDC_CHECKNEWER, BM_SETCHECK, DWORD(updatecheck) ? BST_CHECKED : BST_UNCHECKED, NULL);
+			wstring tsvninstalled = CAppUtils::GetTSVNPath();
+			if (tsvninstalled.empty())
+				::EnableWindow(GetDlgItem(*this, IDC_USETSVN), FALSE);
+			SetDlgItemText(*this, IDC_NUMLOGS, numBuf);
+		}
+		return TRUE;
+	case WM_COMMAND:
+		return DoCommand(LOWORD(wParam));
+	default:
+		return FALSE;
+	}
+}
+
+LRESULT COptionsDlg::DoCommand(int id)
+{
+	switch (id)
+	{
+	case IDOK:
+		{
+			CRegStdWORD regShowTaskbarIcon = CRegStdWORD(_T("Software\\CommitMonitor\\TaskBarIcon"), FALSE);
+			CRegStdString regStartWithWindows = CRegStdString(_T("Software\\Microsoft\\Windows\\CurrentVersion\\Run\\CommitMonitor"));
+			CRegStdWORD regAnimateIcon = CRegStdWORD(_T("Software\\CommitMonitor\\Animate"), TRUE);
+			CRegStdWORD regPlaySound = CRegStdWORD(_T("Software\\CommitMonitor\\PlaySound"), TRUE);
+			CRegStdWORD regUseTSVN = CRegStdWORD(_T("Software\\CommitMonitor\\UseTSVN"), TRUE);
+			CRegStdWORD updatecheck = CRegStdWORD(_T("Software\\CommitMonitor\\CheckNewer"), TRUE);
+			CRegStdWORD numlogs = CRegStdWORD(_T("Software\\CommitMonitor\\NumLogs"), 30);
+			bool bShowTaskbarIcon = !!SendDlgItemMessage(*this, IDC_TASKBAR_ALWAYSON, BM_GETCHECK, 0, NULL);
+			bool bStartWithWindows = !!SendDlgItemMessage(*this, IDC_AUTOSTART, BM_GETCHECK, 0, NULL);
+			bool bAnimateIcon = !!SendDlgItemMessage(*this, IDC_ANIMATEICON, BM_GETCHECK, 0, NULL);
+			bool bPlaySound = !!SendDlgItemMessage(*this, IDC_NOTIFICATIONSOUND, BM_GETCHECK, 0, NULL);
+			bool bUseTSVN = !!SendDlgItemMessage(*this, IDC_USETSVN, BM_GETCHECK, 0, NULL);
+			bool bUpdateCheck = !!SendDlgItemMessage(*this, IDC_CHECKNEWER, BM_GETCHECK, 0, NULL);
+			regShowTaskbarIcon = bShowTaskbarIcon;
+			regAnimateIcon = bAnimateIcon;
+			regPlaySound = bPlaySound;
+			regUseTSVN = bUseTSVN;
+			updatecheck = bUpdateCheck;
+			::SendMessage(m_hHiddenWnd, COMMITMONITOR_CHANGEDINFO, 0, 0);
+			if (bStartWithWindows)
+			{
+				TCHAR buf[MAX_PATH*4];
+				GetModuleFileName(NULL, buf, MAX_PATH*4);
+                wstring cmd = wstring(buf);
+                cmd += _T(" /hidden");
+				regStartWithWindows = cmd;
+			}
+			else
+				regStartWithWindows.removeValue();
+
+			int len = ::GetWindowTextLength(GetDlgItem(*this, IDC_DIFFVIEWER));
+			TCHAR * divi = new TCHAR[len+1];
+			::GetDlgItemText(*this, IDC_DIFFVIEWER, divi, len+1);
+			wstring dv = wstring(divi);
+			delete [] divi;
+			CRegStdString diffViewer = CRegStdString(_T("Software\\CommitMonitor\\DiffViewer"));
+			if (!dv.empty())
+				diffViewer = dv;
+			else
+				diffViewer.removeValue();
+
+			len = ::GetWindowTextLength(GetDlgItem(*this, IDC_NOTIFICATIONSOUNDPATH));
+			divi = new TCHAR[len+1];
+			::GetDlgItemText(*this, IDC_NOTIFICATIONSOUNDPATH, divi, len+1);
+			wstring ns = wstring(divi);
+			delete [] divi;
+			CRegStdString notifySound = CRegStdString(_T("Software\\CommitMonitor\\NotificationSound"));
+			if (!ns.empty())
+				notifySound = ns;
+			else
+				notifySound.removeValue();
+
+			len = ::GetWindowTextLength(GetDlgItem(*this, IDC_NUMLOGS));
+			divi = new TCHAR[len+1];
+			::GetDlgItemText(*this, IDC_NUMLOGS, divi, len+1);
+			DWORD nLogs = _ttol(divi);
+			numlogs = nLogs;
+			delete [] divi;
+		}
+		// fall through
+	case IDCANCEL:
+		EndDialog(*this, id);
+		break;
+	case IDC_DIFFBROWSE:
+		{
+			OPENFILENAME ofn = {0};		// common dialog box structure
+			TCHAR szFile[MAX_PATH] = {0};  // buffer for file name
+			// Initialize OPENFILENAME
+			ofn.lStructSize = sizeof(OPENFILENAME);
+			ofn.hwndOwner = *this;
+			ofn.lpstrFile = szFile;
+			ofn.nMaxFile = sizeof(szFile)/sizeof(TCHAR);
+			ofn.lpstrTitle = _T("Select Diff Viewer...\0");
+			ofn.Flags = OFN_FILEMUSTEXIST|OFN_HIDEREADONLY|OFN_PATHMUSTEXIST|OFN_DONTADDTORECENT;
+			ofn.lpstrFilter = _T("Programs\0*.exe;*.com\0All files\0*.*\0\0");
+			ofn.nFilterIndex = 1;
+			// Display the Open dialog box. 
+			if (GetOpenFileName(&ofn)==TRUE)
+			{
+				SetDlgItemText(*this, IDC_DIFFVIEWER, szFile);
+			}
+		}
+		break;
+	case IDC_SOUNDBROWSE:
+		{
+			OPENFILENAME ofn = {0};		// common dialog box structure
+			TCHAR szFile[MAX_PATH] = {0};  // buffer for file name
+			// Initialize OPENFILENAME
+			ofn.lStructSize = sizeof(OPENFILENAME);
+			ofn.hwndOwner = *this;
+			ofn.lpstrFile = szFile;
+			ofn.nMaxFile = sizeof(szFile)/sizeof(TCHAR);
+			ofn.lpstrTitle = _T("Select Notification Sound...\0");
+			ofn.Flags = OFN_FILEMUSTEXIST|OFN_HIDEREADONLY|OFN_PATHMUSTEXIST|OFN_DONTADDTORECENT;
+			ofn.lpstrFilter = _T("Sound Files\0*.wav;*.mp3\0All files\0*.*\0\0");
+			ofn.nFilterIndex = 1;
+			// Display the Open dialog box. 
+			if (GetOpenFileName(&ofn)==TRUE)
+			{
+				SetDlgItemText(*this, IDC_NOTIFICATIONSOUNDPATH, szFile);
+			}
+		}
+		break;
+	}
+	return 1;
+}
+
---
+++ tags/version-1.5.0/src/UrlInfo.cpp
@@ -0,0 +1,417 @@
+// CommitMonitor - simple checker for new commits in svn repositories
+
+// Copyright (C) 2007-2009 - Stefan Kueng
+
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software Foundation,
+// 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+//
+#include "StdAfx.h"
+#include "UrlInfo.h"
+#include "AppUtils.h"
+#include "MappedInFile.h"
+#include <Wincrypt.h>
+
+#pragma comment(lib, "Crypt32.lib")
+
+CUrlInfo::CUrlInfo(void) : lastchecked(0)
+	, lastcheckedrev(0)
+	, minutesinterval(90)
+	, minminutesinterval(0)
+	, disallowdiffs(false)
+	, parentpath(false)
+{
+}
+
+CUrlInfo::~CUrlInfo(void)
+{
+}
+
+bool CUrlInfo::Save(FILE * hFile)
+{
+	if (!CSerializeUtils::SaveNumber(hFile, URLINFO_VERSION))
+		return false;
+	if (!CSerializeUtils::SaveString(hFile, username))
+		return false;
+	// encrypt the password
+	DATA_BLOB blob, outblob;
+	string encpwd = CUnicodeUtils::StdGetUTF8(password);
+	encpwd = "encrypted_" + encpwd;
+	blob.cbData = encpwd.size();
+	blob.pbData = (BYTE*)encpwd.c_str();
+	if (CryptProtectData(&blob, _T("CommitMonitorLogin"), NULL, NULL, NULL, CRYPTPROTECT_UI_FORBIDDEN, &outblob))
+	{
+		if (!CSerializeUtils::SaveBuffer(hFile, outblob.pbData, outblob.cbData))
+		{
+			LocalFree(outblob.pbData);
+			return false;
+		}
+		LocalFree(outblob.pbData);
+	}
+	if (!CSerializeUtils::SaveString(hFile, name))
+		return false;
+	if (!CSerializeUtils::SaveNumber(hFile, lastchecked))
+		return false;
+	if (!CSerializeUtils::SaveNumber(hFile, lastcheckedrev))
+		return false;
+	if (!CSerializeUtils::SaveNumber(hFile, lastcheckedrobots))
+		return false;
+	if (!CSerializeUtils::SaveNumber(hFile, minutesinterval))
+		return false;
+	if (!CSerializeUtils::SaveNumber(hFile, minminutesinterval))
+		return false;
+	if (!CSerializeUtils::SaveNumber(hFile, disallowdiffs))
+		return false;
+	if (!CSerializeUtils::SaveString(hFile, ignoreUsers))
+		return false;
+	if (!CSerializeUtils::SaveNumber(hFile, parentpath))
+		return false;
+	if (!CSerializeUtils::SaveString(hFile, error))
+		return false;
+	if (!CSerializeUtils::SaveString(hFile, callcommand))
+		return false;
+	if (!CSerializeUtils::SaveNumber(hFile, noexecuteignored))
+		return false;
+	if (!CSerializeUtils::SaveString(hFile, webviewer))
+		return false;
+
+    // prevent caching more than 1000 revisions - this is a commit monitor, not a full featured
+    // log dialog!
+    while (logentries.size() > 1000)
+        logentries.erase(logentries.begin());
+
+	if (!CSerializeUtils::SaveNumber(hFile, CSerializeUtils::SerializeType_Map))
+		return false;
+	if (!CSerializeUtils::SaveNumber(hFile, logentries.size()))
+		return false;
+
+	for (map<svn_revnum_t,SVNLogEntry>::iterator it = logentries.begin(); it != logentries.end(); ++it)
+	{
+		if (!CSerializeUtils::SaveNumber(hFile, it->first))
+			return false;
+		if (!it->second.Save(hFile))
+			return false;
+	}
+	return true;
+}
+
+bool CUrlInfo::Load(const unsigned char *& buf)
+{
+	unsigned __int64 version = 0;
+	if (!CSerializeUtils::LoadNumber(buf, version))
+		return false;
+	unsigned __int64 value = 0;
+	if (!CSerializeUtils::LoadString(buf, username))
+		return false;
+
+	const unsigned char * buf2 = buf;
+	BYTE * pbData = NULL;
+	size_t len = 0;
+	if (!CSerializeUtils::LoadBuffer(buf, pbData, len))
+	{
+		buf = buf2;
+		if (!CSerializeUtils::LoadString(buf, password))
+			return false;
+	}
+
+	// decrypt the password
+	DATA_BLOB blob, outblob;
+	blob.cbData = len;
+	blob.pbData = pbData;
+	if (CryptUnprotectData(&blob, NULL, NULL, NULL, NULL, CRYPTPROTECT_UI_FORBIDDEN, &outblob))
+	{
+		string encpwd = string((const char*)outblob.pbData, outblob.cbData);
+		if (_strnicmp(encpwd.c_str(), "encrypted_", 10) == 0)
+		{
+			encpwd = encpwd.substr(10);
+			password = CUnicodeUtils::StdGetUnicode(encpwd);
+		}
+		LocalFree(outblob.pbData);
+	}
+	delete [] pbData;
+
+	if (!CSerializeUtils::LoadString(buf, name))
+		return false;
+	if (!CSerializeUtils::LoadNumber(buf, value))
+		return false;
+	lastchecked = value;
+	if (!CSerializeUtils::LoadNumber(buf, value))
+		return false;
+	lastcheckedrev = (svn_revnum_t)value;
+	if (version > 1)
+	{
+		if (!CSerializeUtils::LoadNumber(buf, value))
+			return false;
+		lastcheckedrobots = (int)value;
+	}
+	if (!CSerializeUtils::LoadNumber(buf, value))
+		return false;
+	minutesinterval = (int)value;
+	if (version > 1)
+	{
+		if (!CSerializeUtils::LoadNumber(buf, value))
+			return false;
+		minminutesinterval = (int)value;
+	}
+	if (version < 4)
+	{
+		if (!CSerializeUtils::LoadNumber(buf, value))
+			return false;
+	}
+	if (version > 1)
+	{
+		if (!CSerializeUtils::LoadNumber(buf, value))
+			return false;
+		disallowdiffs = !!value;
+	}
+	if (version >= 3)
+	{
+		if (version >= 5)
+		{
+			if (!CSerializeUtils::LoadString(buf, ignoreUsers))
+				return false;
+		}
+		else
+		{
+			if (!CSerializeUtils::LoadNumber(buf, value))
+				return false;
+			if (value)
+				ignoreUsers = username;
+		}
+	}
+
+	if (!CSerializeUtils::LoadNumber(buf, value))
+		return false;
+	parentpath = !!value;
+	if (!CSerializeUtils::LoadString(buf, error))
+		return false;
+	if (version >= 6)
+	{
+		if (!CSerializeUtils::LoadString(buf, callcommand))
+			return false;
+	}
+	if (version >= 8)
+	{
+		if (!CSerializeUtils::LoadNumber(buf, value))
+			return false;
+		noexecuteignored = !!value;
+	}
+	else
+		noexecuteignored = false;
+	if (version >= 7)
+	{
+		if (!CSerializeUtils::LoadString(buf, webviewer))
+			return false;
+	}
+
+	logentries.clear();
+	if (!CSerializeUtils::LoadNumber(buf, value))
+		return false;
+	if (CSerializeUtils::SerializeType_Map == value)
+	{
+		if (CSerializeUtils::LoadNumber(buf, value))
+		{
+			// we had a bug where the size could be bigger than 1000, but
+			// only the first 1000 entries were actually saved.
+			// instead of bailing out if the value is bigger than 1000, we
+			// adjust it to the max saved values instead.
+			// in case the value is out of range for other reasons,
+			// the further serialization should bail out soon enough.
+			if (value > 1000)
+				value = 1000;
+			for (unsigned __int64 i=0; i<value; ++i)
+			{
+				unsigned __int64 key;
+				SVNLogEntry logentry;
+				if (!CSerializeUtils::LoadNumber(buf, key))
+					return false;
+				if (!logentry.Load(buf))
+					return false;
+				logentries[(svn_revnum_t)key] = logentry;
+			}
+			return true;
+		}
+	}
+	return false;
+}
+
+CUrlInfos::CUrlInfos(void)
+{
+}
+
+CUrlInfos::~CUrlInfos(void)
+{
+}
+
+bool CUrlInfos::Load()
+{
+	wstring urlfile = CAppUtils::GetAppDataDir() + _T("\\urls");
+	wstring urlfilebak = CAppUtils::GetAppDataDir() + _T("\\urls_backup");
+	if (!PathFileExists(urlfile.c_str()))
+		return false;
+	if (Load(urlfile.c_str()))
+	{
+		// urls file successfully loaded: create a backup copy
+		CopyFile(urlfile.c_str(), urlfilebak.c_str(), FALSE);
+		return true;
+	}
+	else
+	{
+		// loading the file failed. Check whether there's a backup
+		// file available to load instead
+		return Load(urlfilebak.c_str());
+	}
+}
+
+void CUrlInfos::Save()
+{
+	wstring urlfile = CAppUtils::GetAppDataDir() + _T("\\urls");
+	wstring urlfilenew = CAppUtils::GetAppDataDir() + _T("\\urls_new");
+	if (Save(urlfilenew.c_str()))
+	{
+		DeleteFile(urlfile.c_str());
+		MoveFile(urlfilenew.c_str(), urlfile.c_str());
+	}
+}
+
+bool CUrlInfos::Save(LPCWSTR filename)
+{
+#ifdef _DEBUG
+	DWORD dwStartTicks = GetTickCount();
+#endif
+    FILE * hFile = NULL;
+    _tfopen_s(&hFile, filename, _T("w+b"));
+	if (hFile == NULL)
+		return false;
+    char filebuffer[4096];
+    setvbuf(hFile, filebuffer, _IOFBF, 4096);
+
+	guard.AcquireReaderLock();
+	bool bSuccess = Save(hFile);
+	guard.ReleaseReaderLock();
+	fclose(hFile);
+	if (bSuccess)
+	{
+		// rename the file to the original requested name
+		TRACE(_T("data saved\n"));
+#ifdef _DEBUG
+		TCHAR timerbuf[MAX_PATH] = {0};
+		_stprintf_s(timerbuf, MAX_PATH, _T("time needed for saving all url info: %ld ms\n"), GetTickCount()-dwStartTicks);
+		TRACE(timerbuf);
+#endif
+		return true;
+	}
+	return false;
+}
+
+bool CUrlInfos::Load(LPCWSTR filename)
+{
+	bool bRet = false;
+#ifdef _DEBUG
+	DWORD dwStartTicks = GetTickCount();
+#endif
+	CMappedInFile file(filename);
+    guard.AcquireWriterLock();
+	const unsigned char * buf = file.GetBuffer();
+	if (buf)
+		bRet = Load(buf);
+	guard.ReleaseWriterLock();
+	TRACE(_T("data loaded\n"));
+#ifdef _DEBUG
+	TCHAR timerbuf[MAX_PATH] = {0};
+	_stprintf_s(timerbuf, MAX_PATH, _T("time needed for loading all url info: %ld ms\n"), GetTickCount()-dwStartTicks);
+	TRACE(timerbuf);
+#endif
+	return bRet;
+}
+
+bool CUrlInfos::Save(FILE * hFile)
+{
+	if (!CSerializeUtils::SaveNumber(hFile, URLINFOS_VERSION))
+		return false;
+	// first save the size of the map
+	if (!CSerializeUtils::SaveNumber(hFile, CSerializeUtils::SerializeType_Map))
+		return false;
+	if (!CSerializeUtils::SaveNumber(hFile, infos.size()))
+		return false;
+	for (map<wstring,CUrlInfo>::iterator it = infos.begin(); it != infos.end(); ++it)
+	{
+		if (!CSerializeUtils::SaveString(hFile, it->first))
+			return false;
+		if (!it->second.Save(hFile))
+			return false;
+	}
+	return true;
+}
+
+bool CUrlInfos::Load(const unsigned char *& buf)
+{
+	unsigned __int64 version = 0;
+	if (!CSerializeUtils::LoadNumber(buf, version))
+		return false;
+	infos.clear();
+	unsigned __int64 value = 0;
+	if (!CSerializeUtils::LoadNumber(buf, value))
+		return false;
+	if (CSerializeUtils::SerializeType_Map == value)
+	{
+		if (CSerializeUtils::LoadNumber(buf, value))
+		{
+			for (unsigned __int64 i=0; i<value; ++i)
+			{
+				wstring key;
+				CUrlInfo info;
+				if (!CSerializeUtils::LoadString(buf, key))
+					return false;
+				if (!info.Load(buf))
+					return false;
+				info.url = key;
+				infos[key] = info;
+			}
+			return true;
+		}
+	}
+	return false;
+}
+
+bool CUrlInfos::IsEmpty()
+{
+	bool bIsEmpty = true;
+	guard.AcquireReaderLock();
+	bIsEmpty = (infos.size() == 0);
+	guard.ReleaseReaderLock();
+	return bIsEmpty;
+}
+
+const map<wstring,CUrlInfo> * CUrlInfos::GetReadOnlyData()
+{
+	guard.AcquireReaderLock();
+	return &infos;
+}
+
+map<wstring,CUrlInfo> * CUrlInfos::GetWriteData()
+{
+	guard.AcquireWriterLock();
+	return &infos;
+}
+
+void CUrlInfos::ReleaseReadOnlyData()
+{
+	guard.ReleaseReaderLock();
+}
+
+void CUrlInfos::ReleaseWriteData()
+{
+	guard.ReleaseWriterLock();
+}
+
