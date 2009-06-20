ea9cdad0053006044761bb8684e0af78bfd44c2c
steveking <steveking@095afe11-7fa7-41de-8b0b-c5876c4b31aa>
2009-06-20 13:27:53 +0000

    Tag the 1.6.1 release

---
+++ tags/version-1.6.1/src/UrlInfo.h
@@ -0,0 +1,92 @@
+// CommitMonitor - simple checker for new commits in svn repositories
+
+// Copyright (C) 2007-2009 - Stefan Kueng
+
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software Foundation,
+// 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+//
+#pragma once
+#include <string>
+#include <vector>
+
+#include "SVN.h"
+#include "SerializeUtils.h"
+#include "ReaderWriterLock.h"
+
+#define URLINFO_VERSION		11
+#define URLINFOS_VERSION	1
+
+#define URLINFO_MAXENTRIES 1000
+
+class CUrlInfo
+{
+public:
+	CUrlInfo(void);
+	~CUrlInfo(void);
+
+	wstring						username;
+	wstring						password;
+
+	wstring						url;
+	wstring						name;
+	__time64_t					lastchecked;
+	svn_revnum_t				lastcheckedrev;
+	__time64_t					lastcheckedrobots;
+
+	int							minutesinterval;
+	int							minminutesinterval;
+	bool						disallowdiffs;
+	bool						monitored;
+	wstring						ignoreUsers;
+
+	map<svn_revnum_t,SVNLogEntry> logentries;
+	int							maxentries;
+
+	bool						parentpath;
+	wstring						error;
+	apr_status_t				errNr;
+	wstring						callcommand;
+	bool						noexecuteignored;
+	wstring						webviewer;
+
+	bool						Save(FILE * hFile);
+	bool						Load(const unsigned char *& buf);
+};
+
+class CUrlInfos
+{
+public:
+	CUrlInfos(void);
+	~CUrlInfos(void);
+
+	void						Save();
+	bool						Load();
+	bool						Save(LPCWSTR filename);
+	bool						Load(LPCWSTR filename);
+	bool						IsEmpty();
+
+	const map<wstring,CUrlInfo> *	GetReadOnlyData();
+	map<wstring,CUrlInfo> *		GetWriteData();
+	void						ReleaseReadOnlyData();
+	void						ReleaseWriteData();
+
+protected:
+	bool						Save(FILE * hFile);
+	bool						Load(const unsigned char *& buf);
+
+private:
+	map<wstring,CUrlInfo>		infos;
+	CReaderWriterLock			guard;
+};
+
