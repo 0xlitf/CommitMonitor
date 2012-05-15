b945a789dabab4e4c30ca818ba3f674699c2622b
steveking <steveking@095afe11-7fa7-41de-8b0b-c5876c4b31aa>
2012-05-15 08:30:58 +0000

    tag the 1.8.3 release.

---
+++ tags/version-1.8.3/src/Accurev.h
@@ -0,0 +1,77 @@
+// CommitMonitor - simple checker for new commits in svn repositories
+
+// Copyright (C) 2011 - Stefan Kueng
+
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software Foundation,
+// 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+//
+
+#pragma once
+#include <vector>
+#include <map>
+
+#include "SCCS.h"
+
+#include <string>
+
+using namespace std;
+
+
+class ACCUREV : public SCCS
+{
+public:
+    ACCUREV(void);
+    ~ACCUREV(void);
+
+    void SetAuthInfo(const std::wstring& username, const std::wstring& password);
+
+    bool GetFile(std::wstring sUrl, std::wstring sFile);
+
+    wstring GetRootUrl(const std::wstring& path);
+    size_t GetFileCount() {return (size_t)0;}
+
+    svn_revnum_t GetHEADRevision(const std::wstring& repo, const std::wstring& url);
+
+    bool GetLog(const std::wstring& repo, const std::wstring& url, svn_revnum_t startrev, svn_revnum_t endrev);
+
+    bool Diff(const wstring& url1, svn_revnum_t pegrevision, svn_revnum_t revision1,
+        svn_revnum_t revision2, bool ignoreancestry, bool nodiffdeleted,
+        bool ignorecontenttype,  const wstring& options, bool bAppend,
+        const wstring& outputfile, const wstring& errorfile);
+
+    wstring CanonicalizeURL(const wstring& url);
+    wstring GetLastErrorMsg();
+
+    void SetAndClearProgressInfo(CProgressDlg * pProgressDlg, bool bShowProgressBar = false);
+
+private:
+
+  bool logParser(const wstring& repo, const wstring& url, const wstring& rawLog);
+  bool issueParser(const wstring& rawLog, SCCSLogEntry& logEntry);
+
+  // Accurev command line calls
+  bool AccuLogin(const wstring& username, const wstring& password);
+  bool AccuGetLastPromote(const wstring& repo, const wstring& url, long *pTransactionNo);
+  bool AccuGetHistory(const wstring& repo, const wstring& url, long startrev, long endrev, wstring& rawLog);
+  bool AccuIssueList(const wstring& repo, const wstring& url, long issueNo, wstring& rawLog);
+
+  size_t ExecuteAccurev(std::wstring Parameters, size_t SecondsToWait, wstring& stdOut, wstring& stdErr);
+
+  void ClearErrors();
+  void SetError(const wchar_t *pErrorString);
+
+private:
+  svn_error_t errInt;
+  const wchar_t *pErrorString;
+};
---
+++ tags/version-1.8.3/src/stdafx.h
@@ -0,0 +1,74 @@
+// stdafx.h : include file for standard system include files,
+// or project specific include files that are used frequently, but
+// are changed infrequently
+//
+
+#pragma once
+
+// Modify the following defines if you have to target a platform prior to the ones specified below.
+// Refer to MSDN for the latest info on corresponding values for different platforms.
+#ifndef WINVER                  // Specifies that the minimum required platform is Windows Vista.
+#define WINVER 0x0600           // Change this to the appropriate value to target other versions of Windows.
+#endif
+
+#ifndef _WIN32_WINNT            // Specifies that the minimum required platform is Windows Vista.
+#define _WIN32_WINNT 0x0600     // Change this to the appropriate value to target other versions of Windows.
+#endif
+
+#ifndef _WIN32_WINDOWS          // Specifies that the minimum required platform is Windows 98.
+#define _WIN32_WINDOWS 0x0410   // Change this to the appropriate value to target Windows Me or later.
+#endif
+
+#ifndef _WIN32_IE               // Specifies that the minimum required platform is Internet Explorer 7.0.
+#define _WIN32_IE 0x0700        // Change this to the appropriate value to target other versions of IE.
+#endif
+
+#ifndef NTDDI_VERSION
+#define NTDDI_VERSION NTDDI_LONGHORN
+#endif
+
+// Windows Header Files:
+#include <WinSock2.h>
+#include <Ws2tcpip.h>
+#include <Wspiapi.h>
+#include <windows.h>
+#include <windowsx.h>
+
+// C RunTime Header Files
+#include <stdlib.h>
+#include <malloc.h>
+#include <memory.h>
+#include <tchar.h>
+
+#include <commctrl.h>
+#include <shlwapi.h>
+
+#include "Debug.h"
+
+#pragma comment(lib, "comctl32.lib")
+#pragma comment(lib, "shlwapi.lib")
+
+#ifndef _WIN64
+#   pragma comment(linker, "\"/manifestdependency:type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='X86' publicKeyToken='6595b64144ccf1df' language='*'\"")
+#   ifdef _DEBUG
+#       pragma comment(linker, "\"/manifestdependency:type='win32' name='Microsoft.VC80.CRT' version='8.0.50608.0' processorArchitecture='X86' publicKeyToken='1fc8b3b9a1e18e3b' language='*'\"")
+#   endif
+#else
+#   pragma comment(linker,"/manifestdependency:\"type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='amd64' publicKeyToken='6595b64144ccf1df' language='*'\"")
+#endif
+
+
+#define COMMITMONITOR_POPUP             (WM_APP+1)
+#define COMMITMONITOR_CHANGEDINFO       (WM_APP+2)
+#define COMMITMONITOR_TASKBARCALLBACK   (WM_APP+3)
+#define COMMITMONITOR_REMOVEDURL        (WM_APP+4)
+#define COMMITMONITOR_SAVEINFO          (WM_APP+5)
+#define COMMITMONITOR_FINDMSGPREV       (WM_APP+6)
+#define COMMITMONITOR_FINDMSGNEXT       (WM_APP+7)
+#define COMMITMONITOR_FINDEXIT          (WM_APP+8)
+#define COMMITMONITOR_FINDRESET         (WM_APP+9)
+#define COMMITMONITOR_SETWINDOWHANDLE   (WM_APP+10)
+#define COMMITMONITOR_INFOTEXT          (WM_APP+11)
+#define COMMITMONITOR_GETALL            (WM_APP+12)
+#define COMMITMONITOR_POPUPCLICK        (WM_APP+13)
+#define COMMITMONITOR_LISTCTRLDBLCLICK  (WM_APP+14)
